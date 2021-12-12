910d4a62e382bb7302085e8a0e571c8218e8b290
theroshanron <95roshan1@gmail.com>
2021-12-12 21:13:31 +0530

    For loops

--- nested_loops.py
+++ nested_loops.py
@@ -10,7 +10,7 @@
 # items = [5,2,5,2,2]
 
 # for x in items:
-#     print('x'*x)
+#     print('x'*x)   #Shortcut 
 
 
 
@@ -21,6 +21,6 @@ for x in items:
     output = ''
     # print(output)
     # print(x)
-    for y in range(x):
+    for y in range(x):   # This loop will run five times to add value in output and then two times so on... to store the x string in output
         output += 'x'
     print(output)
