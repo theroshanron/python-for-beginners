4fadcc3c7936e4d749673aa7398762e6ce972ece
theroshanron <95roshan1@gmail.com>
2021-12-13 01:18:33 +0530

    typo

--- Tuples.py
+++ Tuples.py
@@ -1,6 +1,7 @@
-#tuples are immutable , we cannot append or remove anything from tuples
+#tuples are immutable, we cannot append or remove anything from tuples
 
 numbers = (1,2,3)
 num = [1,2,3]
 
 print(numbers.count(2))
+
