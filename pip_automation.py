ae6970a1631ed08354446499a61a5a4bb97c4679
theroshanron <95roshan1@gmail.com>
2021-12-14 19:32:33 +0530

    Tested and work in progress a self made project

---
+++ pip_automation.py
@@ -0,0 +1,15 @@
+from os import path
+import os
+
+from pathlib import Path, PurePath
+
+filename = Path("C:\Users\dh210111\OneDrive - Dunnhumby Ltd\Documents\GitHub\Python programming with mosh\python-for-beginners\pip_intallation\pip.py")
+pythonpath = PurePath(filename)
+# newPath = path.replace(os.sep, '/')
+
+print(pythonpath)
+# A = f'python {newPath}'
+
+# print(A)
+
+""" Work in Progress """
