325785d01b1371f5cbb427cf89fdf647b1e82190
theroshanron <95roshan1@gmail.com>
2021-12-15 01:27:31 +0530

    Pandas exercise

---
+++ ML/ml_project.py
@@ -0,0 +1,14 @@
+#Import CSV file
+#Remove duplicates and clean the data
+#Convert text into numerical values based on project
+#Split the data into training / test sets (A/B testing)
+#Create a Model using ML Algorithm such as decision making
+#Train the model
+#Make predictions
+#Evaluate and Improve
+
+#Popular Libraries we use in Python
+#Numpy
+#Pandas has dataframes for data analysis - Two dimensional data structures
+#MatPlotlib
+#Scikit-learn
