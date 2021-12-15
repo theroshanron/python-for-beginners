34d7382339087c0e165d004d3d40e45f6025dbbd
theroshanron <95roshan1@gmail.com>
2021-12-16 04:40:20 +0530

    Modals

--- Djangoproject/products/urls.py
+++ Djangoproject/products/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('new',views.new_view)
 ]
 
--- Djangoproject/products/views.py
+++ Djangoproject/products/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+
+def new_view(request):
+    return HttpResponse('New Products')
