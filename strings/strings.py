6e0981758ecf7814eae887db9ffaac45c91344ff
theroshanron <95roshan1@gmail.com>
2021-12-17 04:47:01 +0530

    string functions

--- strings/strings.py
+++ strings/strings.py
@@ -3,7 +3,7 @@ course = "Python \"Programming"
 #"" double quotes are mostly preferred for the strings
 # functions - a reusable piece of code to carry out the repetitive tasks
 
-print(len(course))
+print(len(course)) #len to calculate length 
 
 print(course[7])
 print(course[1])
@@ -28,3 +28,17 @@ full_formatted = f"{len(first)} {last}" #formatted text
 
 print(full)
 print(full_formatted)
+
+
+print(course.upper())
+print(course.lower())
+
+print(course.strip())
+
+print(course.find("Py")) #case sensitive and helpful to find index of the string
+
+print(course.replace("P", "C"))
+
+print("th" in course) # to check the existence of letter string in defined variable and returns a boolean
+
+print("swift" not in course) # to check whether it exist in boolean
