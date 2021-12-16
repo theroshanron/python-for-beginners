7428311274a7dd22dc87a4c43c9209ace146ed8e
theroshanron <95roshan1@gmail.com>
2021-12-17 04:36:48 +0530

    Escape Sequences

---
+++ strings/strings.py
@@ -0,0 +1,22 @@
+course = "Python \"Programming"
+# """" quotes are for format of long strings (messages)
+#"" double quotes are mostly preferred for the strings
+# functions - a reusable piece of code to carry out the repetitive tasks
+
+print(len(course))
+
+print(course[7])
+print(course[1])
+print(course[0:3])
+print(course[4:])
+print(course[:])
+print(course[:4])
+
+
+# \ escape character whereas # is comment
+#\" escapes double quote
+# \' escapes single quote
+#\\ includes one backslash
+#\n new line
+
+
