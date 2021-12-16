c7ccea1bfccb065419b2301f40072a3c9090b380
theroshanron <95roshan1@gmail.com>
2021-12-16 05:36:12 +0530

    Adding products to our product page using html and djanl {{}}

--- Djangoproject/products/admin.py
+++ Djangoproject/products/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Discount
 
-admin.site.register(Product)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description','Discount')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Discount, DiscountAdmin)
--- Djangoproject/products/views.py
+++ Djangoproject/products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+from .models import Discount
 
 def index(request):
-    return HttpResponse("Hello world")
-
+    products = Product.objects.all()
+    return render(request, 'index.html',{'products':products})
+   
 
 def new_view(request):
     return HttpResponse('New Products')
+
+
