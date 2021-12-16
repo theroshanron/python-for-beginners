fafcd14131445730ae0f9121e28723e07823f552
theroshanron <95roshan1@gmail.com>
2021-12-17 04:26:22 +0530

    Exercise for beginners

---
+++ exercise_for_beginners/even_number.py
@@ -0,0 +1,12 @@
+# Display even number between 2 and 10 and display the message
+
+count= 0
+for i in range(1,100):
+  
+    if i%2 == 0:
+        print(i)
+        count +=1
+
+
+
+print(f" We have {count} even number")
