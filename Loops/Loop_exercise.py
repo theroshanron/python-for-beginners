5bc4e2b5e2aedd3848fc4619a9581181cf629b70
theroshanron <95roshan1@gmail.com>
2021-12-18 23:42:37 +0530

    Nested loops

--- Loops/Loop_exercise.py
+++ Loops/Loop_exercise.py
@@ -3,6 +3,9 @@
 for number in range(1,10,2):
     print("Attempt",number + 1, (number + 1) * ".")
     
+    
+    
+#For else
 success = True
 for i  in range(7):
     print("Attempt")
@@ -12,3 +15,10 @@ for i  in range(7):
 
 else: 
     print("Attempted but failed")
+    
+    
+#Nested Loops
+
+for x in range (3):
+    for y in range (2):
+        print("Hello",x,y)
