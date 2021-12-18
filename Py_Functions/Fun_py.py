d64ca19099adde234d96db5f216fdaf2ce051153
theroshanron <95roshan1@gmail.com>
2021-12-19 00:16:20 +0530

    Arguments

---
+++ Py_Functions/Fun_py.py
@@ -0,0 +1,9 @@
+#how to write own functions
+#To reduce the lines of code in a single file so that it's easier to maintain and reusable
+
+def greet(first_name,last_name):
+    print(f"Aww! Hello {first_name} {last_name}")
+    print("Welcome")
+
+
+greet("Roshan","Chaudhary")    
