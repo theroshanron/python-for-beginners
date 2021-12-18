db91cc017c7cd0f201b1c50e918f818a9c831b86
theroshanron <95roshan1@gmail.com>
2021-12-18 23:34:28 +0530

    For else

--- Loops/Loop_exercise.py
+++ Loops/Loop_exercise.py
@@ -2,3 +2,13 @@
 
 for number in range(1,10,2):
     print("Attempt",number + 1, (number + 1) * ".")
+    
+success = True
+for i  in range(7):
+    print("Attempt")
+    if success:
+        print("successful")
+        break
+
+else: 
+    print("Attempted but failed")
