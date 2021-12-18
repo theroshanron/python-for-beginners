0798169d008aabb67f5e2768fec6e0349792c9c4
theroshanron <95roshan1@gmail.com>
2021-12-18 23:47:36 +0530

    Iterables

--- Loops/Loop_exercise.py
+++ Loops/Loop_exercise.py
@@ -22,3 +22,23 @@ else:
 for x in range (3):
     for y in range (2):
         print("Hello",x,y)
+        
+#iteration
+print(type(5))
+print(type(range(5)))
+print(range(5))
+
+for x in "Anaconda":
+    print(x)
+    
+for y in range(4):
+    print(y)
+    
+for z in [3,4,5]:
+    print(z)
+    
+#strings, list and range are iterable
+
+shopping_cart = ['colgate','potato','soap']
+for n in shopping_cart:
+    print(n)
